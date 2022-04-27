72adf9550ecd13d754b91d8e702ee78f20ca909d
charlos51ht <36416709+charlos51ht@users.noreply.github.com>
2022-04-27 10:42:14 -0400

    Add files via upload

--- server.java
+++ server.java
@@ -1,7 +1,9 @@
 
 import java.io.*;
+import java.math.BigInteger;
 import java.net.*;
 import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -11,34 +13,37 @@ public class server
 	public static void main(String[] args) throws Exception
 	{
 		try {
-			int b = 3;
+			SecureRandom rand = new SecureRandom();
+			BigInteger b = new BigInteger(rand.nextInt(1000)+"");
 			ServerSocket ss = new ServerSocket(6666);
 			Socket s = ss.accept();
 			System.out.println("Connection established");
 			DataInputStream dis = new DataInputStream(s.getInputStream());
 			OutputStream outToClient = s.getOutputStream();
             DataOutputStream dos = new DataOutputStream(outToClient);
+            
+            String client_key = dis.readUTF();
+            System.out.println(client_key);
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    		keyPairGen.initialize(2048);
+    		KeyPair server_pair = keyPairGen.generateKeyPair();
+    		dos.writeUTF(server_pair.getPublic().getEncoded().toString());
 			
-			int clientG = Integer.parseInt(dis.readUTF()); //public /read 1
-			int clientP = Integer.parseInt(dis.readUTF()); //public /read 2
-			int gamodp = Integer.parseInt(dis.readUTF()); //public number /read 3
+			BigInteger clientG = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public /read 1
+			BigInteger clientP = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public /read 2
+			BigInteger gamodp = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public number /read 3
 			
-			int gbmodp = ((int)Math.pow(clientG, b))%clientP;
-			dos.writeUTF(gbmodp+"");
+			BigInteger gbmodp = clientG.modPow(b,clientP);
+			dos.writeUTF(encrypt(gbmodp+"",client_key));
 			
-			int gabmodp = ((int)Math.pow(gamodp, b))%clientP;
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] key = digest.digest(Integer.toString(gabmodp).getBytes("UTF-8"));
+			BigInteger gabmodp = gamodp.modPow(b,clientP);
+			byte[] key = gabmodp.toByteArray();
 			SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-			String ss_client = dis.readUTF();//read 4
+			String ss_client = decrypt(dis.readUTF(),server_pair.getPrivate());//read 4
 			
-			/*if(gabmodp==ss_client_int)
-				System.out.println("SUCCESS: SHARED SYMMETRIC KEY: "+ss_client);*/
 			System.out.println("aes key: " + ss_client);
-			String decrypted = decrypt(ss_client, secretKeySpec); 
+			String decrypted = decryptLoc(ss_client, secretKeySpec); 
 			System.out.println("Coordinates: " + decrypted);
-			
-			//KeyPai
 			
 			dis.close();
 			dos.close();
@@ -52,7 +57,7 @@ public class server
 		}
 	}
 	
-	public static String decrypt(String encryptedMessage, SecretKeySpec key) throws Exception
+	public static String decryptLoc(String encryptedMessage, SecretKeySpec key) throws Exception
 	{
 		byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMessage);
 		Cipher cipher = Cipher.getInstance("AES");
@@ -60,4 +65,23 @@ public class server
 		byte[] decryptedMessage = cipher.doFinal(encryptedBytes);
 		return new String(decryptedMessage, "UTF8");
 	}
+	public static String encrypt(String message, String key) throws Exception //encryption method
+	{
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = (PublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(key)));
+		byte[] messageBytes = message.getBytes();
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] encrypted = cipher.doFinal(messageBytes);
+		return Base64.getEncoder().encodeToString(encrypted);
+	}
+	public static String decrypt(String ciphertext, PrivateKey key) throws Exception //encryption method
+	{
+		byte[] encryptedBytes = Base64.getDecoder().decode(ciphertext);
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] decrypted = cipher.doFinal(encryptedBytes);
+		return Base64.getEncoder().encodeToString(decrypted);
+	}
+	
 }
