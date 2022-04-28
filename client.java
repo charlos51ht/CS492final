a38f6be7b69da94d80795746bcc1027702eae319
charlos51ht <36416709+charlos51ht@users.noreply.github.com>
2022-04-28 11:51:03 -0400

    Add files via upload

--- client.java
+++ client.java
@@ -2,9 +2,15 @@ import java.io.*;
 import java.math.BigInteger;
 import java.net.*;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -17,34 +23,36 @@ public class client
 	public static int SECRET_KEY = 0;
 	public static void main(String[] args) throws Exception
 	{
-		String location = "41.6931° N, 72.7639° W"; //CCSU coordinates
+		String location = "41.6931 N, 72.7639 W"; //CCSU coordinates
 		try {
 			Socket s = new Socket("localhost",6666);
 			DataOutputStream dos= new DataOutputStream(s.getOutputStream());
 	        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 	        DataInputStream dis = new DataInputStream(s.getInputStream());
-			
+	        
+	        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    		keyPairGen.initialize(4096);
+	        KeyPair client_pair = keyPairGen.generateKeyPair();
+    		dos.writeUTF(Base64.getEncoder().encodeToString(client_pair.getPublic().getEncoded()));//send public key for encryption
+    		String server_key = dis.readUTF();
+    		
 			//Client Side
-			int clientG = 9;//random integer?
-			int clientP = 23;//random integer?
-			int a = 4;
+	        SecureRandom rand = new SecureRandom();
+			BigInteger clientG = BigInteger.probablePrime(128,rand);//random integer?
+			BigInteger clientP = BigInteger.probablePrime(128,rand);
+			BigInteger a = new BigInteger(rand.nextInt(1000)+"");
 			
-			/*dos.writeUTF(key_pair.getPublic().toString());
-			dos.writeUTF(key_pair.getPrivate().toString());*/
-			dos.writeUTF(clientG+""); //send 1
-			dos.writeUTF(clientP+""); //send 2
-			
-			int gamodp = ((int)Math.pow(clientG, a))%clientP;
-			
-			dos.writeUTF(gamodp+""); //send 3
-			
-			int gbmodp = Integer.parseInt(dis.readUTF()); //gets g^b mod p from server
-			int gabmodp = ((int) Math.pow(gbmodp, a))%clientP; //creates the key to encrypt location using server's 
-			
+			dos.writeUTF(encrypt(clientG+"",server_key)); //send 1
+			dos.writeUTF(encrypt(clientP+"",server_key)); //send 2
+			BigInteger gamodp = clientG.modPow(a, clientP);
+			dos.writeUTF(encrypt(gamodp+"",server_key)); //send 3
+			BigInteger gbmodp = new BigInteger(decrypt(dis.readUTF(),client_pair.getPrivate())); //gets g^b mod p from server
+			BigInteger gabmodp = gbmodp.modPow(a, clientP); //creates the key to encrypt location using server's 
+	
 			//String ss_client = Integer.toString(gabmodp);
-			byte[] key = (Integer.toString(gabmodp)).getBytes("UTF-8");
+			byte[] key = gabmodp.toByteArray();
 			SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-			String encryptedLocation = encrypt(location, secretKeySpec);
+			String encryptedLocation = encryptLoc(location, secretKeySpec);
 			dos.writeUTF(encryptedLocation+"");//send 4
 			
 			dis.close();
@@ -63,12 +71,32 @@ public class client
 		}
 	}
 	
-	public static String encrypt(String loc, SecretKeySpec key) throws Exception //encryption method
+	public static String encryptLoc(String loc, SecretKeySpec key) throws Exception //encryption method for shared symmetric key
 	{
 		byte[] locationBytes = loc.getBytes();
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] encrypted = cipher.doFinal(locationBytes);
 		return Base64.getEncoder().encodeToString(encrypted);
+	}
+	public static String encrypt(String message, String key) throws Exception //encryption method
+	{
+		byte[] publicKeyArray = Base64.getDecoder().decode(key);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = (PublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyArray));
+		byte[] messageBytes = message.getBytes();
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		cipher.update(messageBytes);
+		byte[] encrypted = cipher.doFinal();
+		return  Base64.getEncoder().encodeToString(encrypted);
+	}
+	public static String decrypt(String ciphertext, PrivateKey key) throws Exception //encryption method
+	{
+		byte[] encryptedBytes = Base64.getDecoder().decode(ciphertext);
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] decrypted = cipher.doFinal(encryptedBytes);
+		return new String(decrypted);
 	}
 }
--- server.java
+++ server.java
@@ -1,8 +1,9 @@
 
 import java.io.*;
+import java.math.BigInteger;
 import java.net.*;
 import java.security.*;
-import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -12,33 +13,35 @@ public class server
 	public static void main(String[] args) throws Exception
 	{
 		try {
-			int b = 3;
+			SecureRandom rand = new SecureRandom();
+			BigInteger b = new BigInteger(rand.nextInt(1000)+"");
 			ServerSocket ss = new ServerSocket(6666);
 			Socket s = ss.accept();
 			System.out.println("Connection established");
 			DataInputStream dis = new DataInputStream(s.getInputStream());
 			OutputStream outToClient = s.getOutputStream();
             DataOutputStream dos = new DataOutputStream(outToClient);
+    		
+            String client_key = dis.readUTF();
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    		keyPairGen.initialize(4096);
+    		KeyPair server_pair = keyPairGen.generateKeyPair();
+    		dos.writeUTF(Base64.getEncoder().encodeToString(server_pair.getPublic().getEncoded()));
+    		
+			BigInteger clientG = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public /read 1
+			BigInteger clientP = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public /read 2
+			BigInteger gamodp = new BigInteger(decrypt(dis.readUTF(),server_pair.getPrivate())); //public number /read 3
+			BigInteger gbmodp = clientG.modPow(b,clientP);
+			dos.writeUTF(encrypt(gbmodp+"",client_key));
 			
-			int clientG = Integer.parseInt(dis.readUTF()); //public /read 1
-			int clientP = Integer.parseInt(dis.readUTF()); //public /read 2
-			int gamodp = Integer.parseInt(dis.readUTF()); //public number /read 3
-			
-			int gbmodp = ((int)Math.pow(clientG, b))%clientP;
-			dos.writeUTF(gbmodp+"");
-			
-			int gabmodp = ((int)Math.pow(gamodp, b))%clientP;
-			byte[] key = (Integer.toString(gabmodp)).getBytes("UTF-8");
+			BigInteger gabmodp = gamodp.modPow(b,clientP);
+			byte[] key = gabmodp.toByteArray();
 			SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
 			String ss_client = dis.readUTF();//read 4
-			int ss_client_int = Integer.parseInt(ss_client);
-			if(gabmodp==ss_client_int)
-				System.out.println("SUCCESS: SHARED SYMMETRIC KEY: "+ss_client);
-			System.out.println("Encrypted message" + ss_client);
-			String decrypted = decrypt(ss_client, secretKeySpec); 
-			System.out.println("Coordinates: " + decrypted);
 			
-			//KeyPai
+			System.out.println("aes key: " + ss_client);
+			String decrypted = decryptLoc(ss_client, secretKeySpec); 
+			System.out.println("Coordinates: " + decrypted);
 			
 			dis.close();
 			dos.close();
@@ -52,12 +55,33 @@ public class server
 		}
 	}
 	
-	public static String decrypt(String encryptedMessage, SecretKeySpec key) throws Exception
+	public static String decryptLoc(String encryptedMessage, SecretKeySpec key) throws Exception
 	{
 		byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMessage);
 		Cipher cipher = Cipher.getInstance("AES");
 		cipher.init(Cipher.DECRYPT_MODE, key);
 		byte[] decryptedMessage = cipher.doFinal(encryptedBytes);
-		return new String(decryptedMessage, "UTF8");
+		return new String(decryptedMessage);
 	}
+	public static String encrypt(String message,String key) throws Exception //encryption method
+	{
+		byte[] publicKeyArray = Base64.getDecoder().decode(key);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = (PublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyArray));
+		byte[] messageBytes = message.getBytes();
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		cipher.update(messageBytes);
+		byte[] encrypted = cipher.doFinal();
+		return  Base64.getEncoder().encodeToString(encrypted);
+	}
+	public static String decrypt(String ciphertext, PrivateKey key) throws Exception //encryption method
+	{
+		byte[] encryptedBytes = Base64.getDecoder().decode(ciphertext);
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] decrypted = cipher.doFinal(encryptedBytes);
+		return new String(decrypted);
+	}
+	
 }
